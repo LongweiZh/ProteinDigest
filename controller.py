7a07faba6007b5867e0d0e7c42e530120f1d617d
longwei <lz539@georgetown.edu>
2023-11-30 08:02:34 -0500

    Revise files.

--- controller.py
+++ controller.py
@@ -13,7 +13,7 @@
 from tg import expose, TGController
 
 
-class RootController(TGController):
+class Root(TGController):
 	@expose()
 	def index(self):
 		return 'Hello World'
@@ -21,22 +21,3 @@ class RootController(TGController):
 	@expose('hello.xhtml')
 	def hello(self, person=None):
 		return dict(person=person)
-
-
-# Create an actual application
-from tg import MinimalApplicationConfigurator
-
-config = MinimalApplicationConfigurator()
-config.update_blueprint({
-	'root_controller': RootController(),
-	'renderers': ['kajiki']
-})
-
-application = config.make_wsgi_app()
-
-# Serve the application
-from wsgiref.simple_server import make_server
-
-print("Serving on port 8080...")
-httpd = make_server("", 8080, application)
-httpd.serve_forever()
--- start.py
+++ start.py
@@ -10,11 +10,20 @@
 # ===================================
 
 import sys
+#import cherrypy
 from controller import Root
+from tg import AppConfig
+from wsgiref.simple_server import make_server
+import webhelpers2
 
 if __name__ == "__main__":
 	try:
-		turbogears.start_server(Root())
-	except ConfigurationError as exc:
-		sys.stderr.write(str(exc))
-		sys.exit(1)
+		config = AppConfig(minimal=True, root_controller=Root())
+		config.renderers = ['kajiki']
+		config['helpers'] = webhelpers2
+		application = config.make_wsgi_app()
+		httpd = make_server('', 8080, application)
+		httpd.serve_forever()
+	except Exception as e:
+		print("ERROR")
+		print(e)
